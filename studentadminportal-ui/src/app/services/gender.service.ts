64ae5f0adff5a5766aab988f649dd1992660feae
Thierry Sawadogo <38849585+titisawadogo@users.noreply.github.com>
2022-02-20 15:15:09 -0500

    Section 5 done

---
+++ studentadminportal-ui/src/app/services/gender.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Gender } from '../models/api-models/gender.model';
+import { Student } from '../models/api-models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenderService {
+
+  private baseApiUrl = 'https://localhost:5001';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getGenderList(): Observable<Gender[]> {
+    return this.httpClient.get<Gender[]>(this.baseApiUrl + '/gender');
+  }
+}
